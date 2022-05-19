8aa5cedb7ea350e95437ad55d766930c725476c2
Stephan Lüddemann <stephan@MBP-Xcnt.fritz.box>
2022-05-19 11:53:16 +0200

    added useRef for detecting the width of the navigation

--- src/Components/Navigation/Navigation.tsx
+++ src/Components/Navigation/Navigation.tsx
@@ -1,24 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navigation.css";
-import { Button } from "../Button/Button";
+import { Link } from "../Link/Link";
+import Logo from "../../assets/shared/desktop/logo-dark.png";
 
 interface NavigationProps {
   /**
    * navigation handler emitted by the buttons
    */
-  onNavigate: (to: string) => void;
+  onNavigate: (to: string | null | undefined) => void;
 }
 
 export const Navigation = ({ onNavigate, ...props }: NavigationProps) => {
-  function handleNavigate(to: string) {
+  function handleNavigate(to: string | undefined) {
     onNavigate(to);
   }
+  const [width, setWidth] = useState<number | undefined>(0);
+
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref) {
+      window.addEventListener("resize", () => {
+        setWidth(ref.current?.offsetWidth);
+      });
+    }
+
+    return () => {
+      window.removeEventListener("resize", () => {
+        setWidth(ref.current?.offsetWidth);
+      });
+    };
+  }, []);
 
   return (
-    <div className="navigation" {...props}>
-      <Button label="our company" onClick={() => handleNavigate("company")} />
-      <Button label="locations" onClick={() => handleNavigate("locations")} />
-      <Button label="contact" onClick={() => handleNavigate("contact")} />
+    <div className="navigation" {...props} ref={ref}>
+      <img src={Logo} alt="Designo Logo" width="202" />
+      <div className="navigation__items">
+        <Link
+          label="our company"
+          color="dark"
+          onClick={() => handleNavigate("company")}
+        />
+        <Link
+          label="locations"
+          color="dark"
+          onClick={() => handleNavigate("locations")}
+        />
+        <Link
+          label="contact"
+          color="dark"
+          onClick={() => handleNavigate("contact")}
+        />
+      </div>
     </div>
   );
 };
